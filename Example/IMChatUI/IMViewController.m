edd943d9478ed1f353d6d1eaaa38de09a4aad5c2
man <963292639@qq.com>
2019-07-04 14:36:56 +0800

    Initial commit

---
+++ Example/IMChatUI/IMViewController.m
@@ -0,0 +1,29 @@
+//
+//  IMViewController.m
+//  IMChatUI
+//
+//  Created by man on 07/04/2019.
+//  Copyright (c) 2019 man. All rights reserved.
+//
+
+#import "IMViewController.h"
+
+@interface IMViewController ()
+
+@end
+
+@implementation IMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
