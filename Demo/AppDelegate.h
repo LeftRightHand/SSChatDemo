c46b7f1d5d4735ccbfc046135cd356cb68a228d6
man <963292639@qq.com>
2019-07-08 15:20:36 +0800

    no message

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by iOS Developer on 2019/7/4.
+//  Copyright © 2019 iOS Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
